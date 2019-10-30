dd91d945eae1f3cedbd8155f56bee8000c674869
Vitalii <romanchuk.vitalij@gmail.com>
2019-10-30 22:59:09 +0200

    feat(models/category): increase number of fields

--- src/server/models/category.ts
+++ src/server/models/category.ts
@@ -1,3 +1,9 @@
 export interface Category {
+    Ordering: number,
+    ShortName: string,
+    HasTarifItem: boolean,
+    ParentID: number,
+    HierarchyLevel: number,
+    ID: number,
     Name: string;
 }
