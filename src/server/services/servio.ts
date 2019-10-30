485c21107c36eb4a875dc452ac900a1b8f693e3d
Vitalii <romanchuk.vitalij@gmail.com>
2019-10-30 20:19:21 +0200

    feat: add verbose logging

--- src/server/services/servio.ts
+++ src/server/services/servio.ts
@@ -10,46 +10,65 @@ import {
 } from "../models/servio/bill";
 import {Category} from "../models/category";
 import {Product} from "../models/product";
+import {logger} from './logger';
+import {Logger} from "winston";
 
 export class ServioService {
     private readonly _apiService: ServioApiService;
+    private readonly _logger: Logger;
 
     constructor(servioUrl: string) {
         this._apiService = new ServioApiService(servioUrl);
+        this._logger = logger;
     }
 
     public async GetCategories(): Promise<Category[]> {
+        this._logger.verbose(`(servio): get categories`);
+
         return this._apiService.GetTarifItems();
     }
 
     public async GetProducts(categoryId: number): Promise<Product[]> {
+        this._logger.verbose(`(servio): get product by id: ${categoryId}`);
+
         return this._apiService.GetTarifItem(categoryId);
     }
 
     public async GetBill(billId: number): Promise<GetBillResponse> {
+        this._logger.verbose(`(servio): get bill [${billId}]`);
+
         return this._apiService.GetBill(billId);
     }
 
     public async CreateBill(type: BillType): Promise<SetBillResponse> {
-        console.log(`SERVIO CREATE BILL TYPE: ${type}`);
+        this._logger.verbose(`(servio): create bill with type [${JSON.stringify(type)}]`);
+
         return this._apiService.SetBill(BillSetOperationType.CreateBill, type, 0, 'Tester');
     }
 
     public async ChangeBillType(billId: number, type: BillType): Promise<SetBillResponse> {
+        this._logger.verbose(`(servio): change bill [${billId}] type [${JSON.stringify(type)}]`);
+
         return this._apiService.SetBill(BillSetOperationType.ChangeBill, type, billId, 'Tester');
     }
 
     public async AddItemToBill(item: BillItem) {
+        this._logger.verbose(`(servio): add bill item [${JSON.stringify(item)}]`);
+
         let setBillItem = <SetBillItem>{...item, operationType: BillSetItemOperationType.AddNewItem};
         return await this._apiService.SetBillItem(setBillItem);
     }
 
     public async ChangeItemInBill(item: BillItem) {
+        this._logger.verbose(`(servio): change bill item [${JSON.stringify(item)}]`);
+
         let setBillItem = <SetBillItem>{...item, operationType: BillSetItemOperationType.ChangeExistedItem};
         return await this._apiService.SetBillItem(setBillItem);
     }
 
     public async DeleteItemFromBill(item: BillItem) {
+        this._logger.verbose(`(servio): delete bill item [${JSON.stringify(item)}]`);
+
         let setBillItem = <SetBillItem>{...item, operationType: BillSetItemOperationType.DeleteExistedItem};
         return await this._apiService.SetBillItem(setBillItem);
     }
