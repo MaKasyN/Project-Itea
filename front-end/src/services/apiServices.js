aacc163ea0e046be2d3c550001fb219b7ca58956
Vitalii <romanchuk.vitalij@gmail.com>
2019-11-03 16:24:48 +0200

    feat: separate building process for dev and production environments

--- front-end/src/services/apiServices.js
+++ front-end/src/services/apiServices.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 //TODO: this setting should be loaded from environment variable or any other dynamic source
-const VUE_APP_API_ENDPOINT = 'http://localhost'; /* process.env.VUE_APP_API_ENDPOINT */
+const VUE_APP_API_ENDPOINT = process.env.VUE_APP_API_ENDPOINT;
 
 axios.defaults.baseURL = VUE_APP_API_ENDPOINT;
 
 const apiServise = {
     async getCategoryList () {
-        console.log(`current endpoint from .env is: ${process.env.VUE_APP_API_ENDPOINT}`);
+        console.log(`main endpoint url is: ${VUE_APP_API_ENDPOINT}`);
         const categoryList = await axios.get(`${VUE_APP_API_ENDPOINT}/api/categories`);
         return categoryList.data;
     },
